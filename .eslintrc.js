fe028a9257f1e1d257ade8b33df47eb4430ac0a7
Anthony Grove <agrove87@gmail.com>
2023-04-01 22:51:36 -0700

    fix rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ const globalRules = {
   'object-curly-newline': 'off', // use prettier instead
   semi: ['error', 'never'],
   'max-len': ['error', { code: 150 }],
+  'implicit-arrow-linebreak': 'off', // use prettier instead
   'import/no-extraneous-dependencies': [
     'error',
     {
